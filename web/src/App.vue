b3dec0d60abe5e33bde2ab359f947d19cd7a92ac
huntersxy <32588548+huntersxy@users.noreply.github.com>
2024-05-22 18:47:20 +0800

    Update App.vue

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
-
+import { useCounterStore } from '@/stores/counter'
+const counter = useCounterStore()
 </script>
 
 <script>
@@ -35,4 +36,11 @@ export default {
   </header>
 
   <RouterView />
+    <div class="toolbox">
+    <Text>Debug box</Text>
+    <RouterLink to="/">主页 </RouterLink>
+    <RouterLink to="/about">个人界面</RouterLink>
+    <button @click="counter.increment">counter +1</button>
+    <div class="counter">Count: {{ counter.count }}</div>
+  </div>
 </template>
