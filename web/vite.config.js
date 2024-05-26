9de8f499cbc32ca9f3e4b7cb40e0e4b3640b25a6
huntersxy <1833505972@qq.com>
2024-05-26 15:19:46 +0800

    代理&个人界面

--- web/vite.config.js
+++ web/vite.config.js
@@ -6,8 +6,27 @@ import VueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/bocchi': {
+        target: 'http://127.0.0.1:10001',
+        ws: true,// 是否代理websocket
+        changeOrigin: true, // 是否将主机头的源更改为目标URL,允许跨域
+        pathRewrite: {
+          '^/bocchi': '/' // 路径重写，将'/api'替换为空串
+        }
+      }
+    }
+  },
   plugins: [
-    vue(),
+    vue({
+      template: {
+        compilerOptions: {
+          // 所有以 mdui- 开头的标签名都是 mdui 组件
+          isCustomElement: (tag) => tag.startsWith('mdui-')
+        }
+      }
+    }),
     VueDevTools(),
   ],
   resolve: {
