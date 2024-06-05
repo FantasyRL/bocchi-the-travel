027fc44968364ae019f299d430e7430912bd7a3e
huntersxy <32588548+huntersxy@users.noreply.github.com>
2024-06-05 05:38:06 +0000

    Deploying to gh-pages from @ FantasyRL/bocchi-the-travel@167f63aae52315c523545269feb9ef107ceffef4 🚀

--- assets/travels-BNa3QYP2.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,r as s,o as a,c as n,b as c}from"./index-5RltvUIs.js";const _={},l={class:"travels"};function d(o,e){const r=s("a-page-header");return a(),n("div",l,[c(r,{style:{border:"1px solid rgb(235, 237, 240)"},title:"我的行程",onBack:e[0]||(e[0]=()=>o.$router.go(-1))})])}const i=t(_,[["render",d]]);export{i as default};
