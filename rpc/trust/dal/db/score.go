8d390022a1a35d78791af2fd06ee5cca82266805
FantasyRL <1845983502@qq.com>
2024-06-09 11:12:17 +0800

    fix

--- rpc/trust/dal/db/score.go
+++ rpc/trust/dal/db/score.go
@@ -70,7 +70,11 @@ func UpdateScore(ctx context.Context, uid int64, score float64) error {
 
 func GetScoreByUId(ctx context.Context, uid int64) (*Score, error) {
 	scoreResp := new(Score)
-	if err := DBScore.WithContext(ctx).Where("uid = ?", uid).Find(scoreResp).Error; err != nil {
+	err := DBScore.WithContext(ctx).Where("uid = ?", uid).Find(scoreResp).Error
+	if errors.Is(err, gorm.ErrRecordNotFound) {
+		return &Score{Uid: uid, Score: 0, Count: 0}, nil
+	}
+	if err != nil {
 		return nil, err
 	}
 	return scoreResp, nil
