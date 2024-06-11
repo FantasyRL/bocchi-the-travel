3a10a854b00073c655fbe75606604848bf728ae0
huntersxy <1833505972@qq.com>
2024-06-11 16:24:34 +0800

    1

--- web/src/views/itinerary.vue
+++ web/src/views/itinerary.vue
@@ -33,17 +33,14 @@ export default {
       datago:null,
       datadata:null,
       id: 1, // 假设这是 itinerary 的 id
-      party: {
-        start_time: "2006-01-02",
-        end_time: "2006-01-03"
-      },
+      party: {},
       info: {},
       partynull: false // 假设这是 party 是否为空的标志
     };
   },
   methods: {
     gogogo(){
-      window.location.replace("//uri.amap.com/navigation?from="+this.datadata+",startpoint&to="+this.datago+",endpoint&via=&mode=car&policy=1&src=mypage&coordinate=gaode&callnative=0") 
+      window.location.replace("https://uri.amap.com/navigation?from="+this.datadata+",startpoint&to="+this.datago+",endpoint&via=&mode=car&policy=1&src=mypage&coordinate=gaode&callnative=0") 
     },
     deleteItinerary() {
       const url = "https://api.xiey.work/bocchi/party/itinerary/delete?itinerary_id=" + this.id; // 假设这是删除 itinerary 的 API 接口地址
--- web/src/views/party.vue
+++ web/src/views/party.vue
@@ -29,50 +29,8 @@ export default {
     return {
       partynull: 0,
       id: 1, // 假设这是 party 的 id
-      infodata: {
-        id: 5,
-        founder_id: 6,
-        title: "1",
-        content: "1",
-        type: "",
-        province: "1",
-        city: "1",
-        start_time: "2006-01-02",
-        end_time: "2006-01-02",
-        member_count: 0,
-        status: 0,
-        rectangle: ""
-      }, // 假设这是 party 的数据对象
-      items: [
-        {
-          id: 5,
-          title: "第一个",
-          founder_id: 6,
-          action_type: 2,
-          rectangle: "1",
-          route_json: "1",
-          remark: "1hhhhhhhhhhhhhhhhhhhhhhhhhh",
-          sequence: 0,
-          schedule_start_time: "2006-01-02 15:40",
-          schedule_end_time: "2006-01-02 15:40",
-          party_id: 5,
-          is_merged: 1
-        },
-        {
-          id: 6,
-          title: "吃饭",
-          founder_id: 6,
-          action_type: 4,
-          rectangle: "1",
-          route_json: "1",
-          remark: "1",
-          sequence: 0,
-          schedule_start_time: "2006-01-02 15:40",
-          schedule_end_time: "2006-01-02 15:40",
-          party_id: 5,
-          is_merged: 1
-        }
-      ]
+      infodata: {}, // 假设这是 party 的数据对象
+      items: []
     };
   },
   methods: {
